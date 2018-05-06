788cd5a20a8e3b933844bfeda0e9164a00329c2a
novokrest <novokrest013@gmail.com>
2018-05-06 18:32:22 +0300

    naming: Add functions for names normalization.

--- naming.py
+++ naming.py
@@ -2,3 +2,12 @@ from werkzeug.utils import secure_filename
 
 def normalize_username(username: str) -> str:
     return secure_filename(username)
+
+def normalize_filename(filename: str) -> str:
+    return secure_filename(filename).lower()
+
+def normalize_bot_name(bot_name: str) -> str:
+    return secure_filename(bot_name).lower()
+
+def get_bot_filename(username: str, filename: str) -> str:
+    return '{}-{}'.format(normalize_username(username), normalize_filename(filename))
