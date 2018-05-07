f4ab31686b8a86f179bce696570a379081815fed
novokrest <novokrest013@gmail.com>
2018-05-07 08:31:55 +0300

    naming: Make bot_image_name different from bot_name
    Fixes #3

--- naming.py
+++ naming.py
@@ -1,5 +1,7 @@
 from werkzeug.utils import secure_filename
 
+BOT_IMAGE_PREFIX = 'zulip-'
+
 def normalize_username(username: str) -> str:
     return secure_filename(username)
 
@@ -13,8 +15,7 @@ def get_bot_name(username: str, name: str) -> str:
     return '{}-{}'.format(normalize_username(username), normalize_bot_name(name))
 
 def get_bot_image_name(bot_name: str) -> str:
-    # for backward compability bot_image_name equals to bot_name (temporary)
-    return bot_name
+    return BOT_IMAGE_PREFIX + bot_name
 
 def extract_bot_name_from_image(bot_image_name: str) -> str:
-    return bot_image_name
+    return bot_image_name[len(BOT_IMAGE_PREFIX):]
